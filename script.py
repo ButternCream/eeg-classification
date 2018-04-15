f836d712b63a845fed924a106b0ad5554c4adea2
rodrigo <estrellar@github.com>
2018-04-15 15:06:18 -0700

    added predict to neural net script; still needs test

--- script.py
+++ script.py
@@ -17,23 +17,24 @@ print('Loaded')
 
 
 # simpler format for building your arrays
-x_one = np.array(a_train)
-x_two = np.array(c_train)
-x = np.concatenate((x_one, x_two))
+train_one = np.array(a_train)
+train_two = np.array(c_train)
+train = np.concatenate((train_one, train_two))
 
-y_one = np.ones((x_one.shape[0], 1)) # create array of ones for those in alc
-y_two = np.zeros((x_two.shape[0], 1)) # create array of 0's for those in control
-y = np.concatenate((y_one, y_two)) # concat in the same order as x
-x = np.expand_dims(x, axis=3)
-print("x shape: ", x.shape)
-print("y shape: ", y.shape)
+
+labels_tr_one = np.ones((train_one.shape[0], 1)) # create array of ones for those in alc
+labels_tr_two = np.zeros((train_two.shape[0], 1)) # create array of 0's for those in control
+labels_train = np.concatenate((labels_tr_one, labels_tr_two)) # concat in the same order as x
+train = np.expand_dims(x, axis=3)
+print("train shape: ", train.shape)
+print("labels_train shape: ", labels_train.shape)
 
 # normalize x
-x = x / 255
+train = train / 255
 
 # build our model. The example provided by Boaz/Mathew requires a base model, this builds a model without a base provided. 
 model = Sequential()
-model.add(Dense(32, input_shape=x.shape[1:]))
+model.add(Dense(32, input_shape=train.shape[1:]))
 model.add(Activation('relu'))
 model.add(Flatten()) # must flatten to ensure we have a 1d array going into our prediction layer
 model.add(Dense(1))
@@ -49,5 +50,25 @@ print("model compiled")
 from keras.callbacks import EarlyStopping
 my_callbacks = [EarlyStopping(monitor='acc', patience=5,mode=max)]
 
-model.fit(x, y, epochs=30, batch_size=32, callbacks=my_callbacks)
+model.fit(train, labels_train, epochs=30, batch_size=32, callbacks=my_callbacks)
+
+# simpler format for building your arrays
+test_one = np.array(a_test)
+test_two = np.array(c_test)
+test = np.concatenate((test_one, test_two))
+
+
+labels_t_one = np.ones((test_one.shape[0], 1)) # create array of ones for those in alc
+labels_t_two = np.zeros((test_two.shape[0], 1)) # create array of 0's for those in control
+labels_test = np.concatenate((labels_t_one, labels_t_two)) # concat in the same order as x
+test = np.expand_dims(x, axis=3)
+print("test shape: ", test.shape)
+print("labels_test shape: ", labels_test.shape)
+
+# normalize test
+test = test / 255
+
+
+print("Testing using predictions...")
+model.predict(test, labels_test, batch_size=32, verbose=1)
 
